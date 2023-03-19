67502869008ada80d72dd70636135ff575105d33
ergunDevv <ergncyln2127088@gmail.com>
2023-03-19 13:39:55 +0300

    Build bug solved

--- src/scenes/footer/index.tsx
+++ src/scenes/footer/index.tsx
@@ -1,6 +1,11 @@
 import Logo from "@/assets/Logo.png";
+import { SelectedPage } from "@/shared/types";
 
-const Footer = () => {
+type Props = {
+
+  setSelectedPage:(value:SelectedPage)=>void
+}
+const Footer = (setSelectedPage: Props) => {
   return (
     <footer className="bg-primary-100 py-16">
       <div className="justify-content mx-auto w-5/6 gap-16 md:flex">
